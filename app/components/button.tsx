6daefa82f44bc346a2891e74d6680345ccde7378
Sergio Lissanou <sergitodelavega@gmail.com>
2026-01-13 16:14:46 +0100

    buttons with shared state

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+interface ButtonProps {
+  count: number;
+  onClick: () => void;
+}
 
-export default function CountButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 100);
-  }
+export default function CountButton({count, onClick} : ButtonProps) {
 
   return (
     <button 
-        onClick={handleClick} 
+        onClick={onClick} 
         className="bg-blue-500 hover:bg-blue-700 text-white rounded font-bold px-4 py-2 mt-2">
       I have clicked {count} times
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
+import { useState } from "react";
 import CountButton from "./components/button";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count + 1);
+  }
+  
   return (
     <div className="flex justify-center items-center flex-col bg-zinc-50 font-sans p-24 dark:bg-black">
       <h1>React Count Button</h1>
-      <CountButton />
+      <CountButton count={count} onClick={handleClick} />
+      <CountButton count={count} onClick={handleClick} />
     </div>
   );
 }
